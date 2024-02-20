72e0cc1a8bb1d2ed1afe8b81e87e6b2a2a80de59
oidahoch3 <132847925+oidahoch3@users.noreply.github.com>
2024-02-20 13:47:00 +0100

    added a button

--- scripts/poepie.js
+++ scripts/poepie.js
@@ -5,6 +5,18 @@ myImage.addEventListener('click', () => {
   if (mySrc === 'images/image_1.jpg') {
     myImage.setAttribute('src', 'images/image_2.jpg');
   } else {
-    myImage.setAttribute('src','images/image_1.jpg' );
+    myImage.setAttribute('src', 'images/image_1.jpg');
   }
+});
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setNumber() {
+  const number = prompt('Please enter a Number');
+  myHeading.textContent = `You owe me ${number} kisses`;
+}
+
+myButton.addEventListener('click', () => {
+  setNumber();
 });
